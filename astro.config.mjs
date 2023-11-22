1c67122efbc868c8d36c646cf6fee3741b67424c
Tony S <sanjayatony@gmail.com>
2023-11-22 15:28:52 +0700

    add adapter Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: vercel()
 });
